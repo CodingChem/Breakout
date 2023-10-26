80973c6b5c2515f6050e026bdeea6c1a03166d8d
Vegard Seines <vegsei@gmail.com>
2023-10-26 11:52:22 +0200

    defined addition operation on vectors

--- game/pieces/vector.py
+++ game/pieces/vector.py
@@ -8,3 +8,10 @@ class Vector:
             y: int
         ) -> None:
         self.x, self.y = x, y
+
+
+    def __add__(
+            self: Self,
+            other: Self
+    ) -> Self:
+        return Vector(self.x + other.x, self.y + other.y)
