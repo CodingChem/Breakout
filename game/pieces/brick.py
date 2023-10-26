754120b3241c4151b54044bf2144e87bd1e560eb
Vegard Seines <vegsei@gmail.com>
2023-10-26 10:45:49 +0200

    Brick is now inheriting Rect instead of being composed of Rect.

--- game/pieces/brick.py
+++ game/pieces/brick.py
@@ -3,7 +3,7 @@ from pygame.draw import rect as draw_rect
 from random import randint
 from .color import Color
 
-class Brick:
+class Brick(Rect):
     def __init__(
             self,
             x: int,
@@ -21,13 +21,13 @@ class Brick:
             height (int): The height of the brick. Defaults to 20
             color (tuple[int,int,int], optional): _description_. Defaults to (random.randint(0,255), random.randint(0,255), random.randint(0,255)).
         """
+        super().__init__(x, y, width, height)
         if color is None:
             self.color = Color(None, randint(0,255), randint(0,255), randint(0,255))
         else:
             self.color = color
 
-
     def draw(self, screen: SurfaceType) -> None:
         """Draw the brick on the screen
         """
-        draw_rect(screen, self.color.to_tuple(), self.rect)
+        draw_rect(screen, self.color.to_tuple(), self)
